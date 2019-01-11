a0c87fb7f51792166a191a22558e9b9021743909
Yaroslav <35149052+YaroslavBoiko@users.noreply.github.com>
2019-01-11 13:00:17 +0200

    Add files via upload

---
+++ FlowerShop/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import сlient.Client;
+
+public class Main {
+    public static void main(String[] args) {
+        Client Jack  = new Client();
+        System.out.println("Good evening, I hope to see you again");
+
+        Client Melania = new Client();
+        System.out.println("Good evening, I hope to see you again");
+        System.out.println(Melania.order.flowers.toString());
+    }
+
+}
